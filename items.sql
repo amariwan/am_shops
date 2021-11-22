9888a58be6ce0ac4d10223137c8339cc9e99b3ac
Aland Mariwan <dev@aland-mariwan.de>
2021-11-22 12:11:01 +0100

    Shops for FiveM GTA RP

---
+++ items.sql
@@ -0,0 +1,31 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('cocacola', 'Coca Cola', -1, 0, 1),
+	('fanta', 'Fanta Exotic', -1, 0, 1),
+	('sprite', 'Sprite', -1, 0, 1),
+	('coffee', 'Coffee', -1, 0, 1),
+	
+	('beer', 'Beer', -1, 0, 1),
+	('plzen', 'Plzen', -1, 0, 1),
+	('wine', 'Wine', -1, 0, 1),
+	('vodka', 'Vodka', -1, 0, 1),
+	('tatra', 'Tatra', -1, 0, 1),
+	('rhum', 'Rhum', -1, 0, 1),
+	('limonade', 'Limonade', -1, 0, 1),
+	('grand_cru', 'Grand_cru', -1, 0, 1),
+	('golem', 'Golem', -1, 0, 1),
+	('champagne', 'Champagne', -1, 0, 1),
+
+	('bolchips', 'Bolchips', -1, 0, 1),
+	('bolpistache', 'Bolpistache', -1, 0, 1),
+	('bolcacahuetes', 'Bolcacahuetes', -1, 0, 1),
+	('bolnoixcajou', 'Bolnoixcajou', -1, 0, 1),
+
+	('pizza_salam', 'Pizza salam', -1, 0, 1),
+	('pizza_mozzarella', 'Pizza mozzarella', -1, 0, 1),
+	('lasagne', 'Lasagne', -1, 0, 1),
+	('salat', 'Salat', -1, 0, 1),
+	
+	('cigarett', 'Cigarette', -1, 0, 1),
+	('rarecigar', 'Rarecigar', -1, 0, 1),
+	('lotteryticket', 'Trisslott', -1, 0, 1)
+;
